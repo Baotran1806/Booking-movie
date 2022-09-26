920c00edfd5ecda2efd769e5b11d7593c47ce1e1
Nyakkon <kyuubi.yae@wibu.me>
2022-09-26 12:41:13 +0700

    first upload

---
+++ src/util/settings/config.js
@@ -0,0 +1,5 @@
+//Định nghĩa các tham số cố định trong dự án
+export const DOMAIN = "https://movie0706.cybersoft.edu.vn";
+export const TOKEN="accessToken";
+export const GROUPID = "GP01";
+export const USER_LOGIN = "USER_LOGIN";
